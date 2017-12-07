19ee99f608fdaf0c99019c8db1186e70c52b23c0
dcodeh <dcodyburrows@gmail.com>
2017-12-07 00:11:35 -0500

    Problem
    I think the distances to Polaris are overflowing...

--- src/Polaris.java
+++ src/Polaris.java
@@ -6,7 +6,8 @@
 public class Polaris extends Location {
 	
 	// in millions of km
-	private static final double DIST_FROM_EARTH = 4096000000.0;
+	
+	private static final double DIST_FROM_EARTH = 4104060000.0;
 	
 	public Polaris() {
 		super(DIST_FROM_EARTH);
